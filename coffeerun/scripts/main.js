6d1c9142e5cb531c64a3eb9736fa5632244c12b4
CHuante <32277113+CHuante@users.noreply.github.com>
2020-10-23 21:59:44 -0700

    Add files via upload
    Changes were made in the .html to allow fire base's database

---
+++ coffeerun/scripts/main.js
@@ -0,0 +1,32 @@
+(function (window) {
+    'use strict';
+    var FORM_SELECTOR = '[data-coffee-order="form"]';
+    var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
+    //  replace with fire vvv
+    var SERVER_URL = 'http://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders';
+    var App = window.App;
+    var Truck = App.Truck;
+    var DataStore = App.DataStore;
+    var RemoteDataStore = App.RemoteDataStore;
+    var FormHandler = App.FormHandler;
+    var Validation = App.Validation;
+    var CheckList = App.CheckList;
+
+    // ch 13 stuff pg 272
+    var remoteDS = new RemoteDataStore(SERVER_URL);
+    var myTruck = new Truck('ncc-1701', remoteDS);
+    
+    window.myTruck = myTruck;
+    var checkList = new CheckList(CHECKLIST_SELECTOR);
+    
+    checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
+    var formHandler = new FormHandler(FORM_SELECTOR);
+
+    formHandler.addSubmitHandler(function (data) {
+        myTruck.createOrder.call(myTruck, data);
+        checkList.addRow.call(checkList, data);
+    });
+
+    formHandler.addInputHandler(Validation.isCompanyEmail);
+
+})(window);
